af7468b745b388889665005768e77649c9c0614a
Marvel <marvel.amgad@outlook.com>
2015-05-25 12:37:01 +0200

    Merge remote-tracking branch 'origin/master'

--- Database/Migrations/2015_03_19_084930_create_menu_items_table.php
+++ Database/Migrations/2015_03_19_084930_create_menu_items_table.php
@@ -16,7 +16,8 @@ class CreateMenuItemsTable extends Migration
 		{
 			Schema::create('menu_items', function(Blueprint $table) {
 				$table->bigIncrements('id');
-				$table->bigInteger('menu_id')->unsigned();	
+				$table->bigInteger('menu_id')->unsigned();
+				$table->foreign('menu_id')->references('id')->on('menus');
 				$table->string('title', 150)->index();
 				$table->string('link', 150)->default('#')->index();
 				$table->enum('status', ['published', 'unpublished'])->default('published');
